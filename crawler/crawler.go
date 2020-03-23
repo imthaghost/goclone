882c29e79ff26ce408f521ae73f81614d40515b3
imthaghost <imthaghost@protonmail.com>
2020-03-23 14:20:14 -0700

    update typo

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,6 +1,6 @@
 package crawler
 
-// Crawl asks the neccessary crawlers for collecting links for building the web page
+// Crawl asks the necessary crawlers for collecting links for building the web page
 func Crawl(site string, projectPath string) {
 	// searches for css, js, and images within a given link
 	Collector(site, projectPath)
