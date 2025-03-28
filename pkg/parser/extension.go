a6a37b2b093d8769445f600865576c172f11c735
Juan Mesaglio <juan.mesaglio@gmail.com>
2025-03-28 19:43:17 -0300

    fix: preserve original filename when saving assets
    Signed-off-by: Juan Mesaglio <juan.mesaglio@gmail.com>

--- pkg/parser/extension.go
+++ pkg/parser/extension.go
@@ -1,39 +1,18 @@
 package parser
 
 import (
+	"net/url"
 	"path"
 )
 
 // URLExtension returns the extension from a given URL, or an empty string
 func URLExtension(URL string) string {
-	/*
-		>>> https://tesla.com/main.css
-		<<< ".css"
-
-		>>> https://tesla.com/main.css?Asf341
-		<<< ".css"
-
-		>>> https://dribbble.com/css/home
-		<<< ""
-	*/
-
-	// get the raw extension from the URL
-	ext := path.Ext(URL)
-	// fmt.Println(ext)
-
-	// check if the extension has more than 5 characters, we need to remove any excess
-	if len(ext) > 5 {
-		// for every index and letter after the 0 index (ext[0] is ".", we want to keep that)
-		for i, char := range ext[1:] {
-			// if the unicode value of the char is not within the bounds alpha chars
-			if !('a' <= char && char <= 'z') || ('A' <= char && char <= 'Z') {
-				// reinitialize the ext to only include the characters for the extension and break
-				ext = ext[:i+1]
-				break
-			}
-		}
+	// Parse the URL
+	u, err := url.Parse(URL)
+	if err != nil {
+		return ""
 	}
 
-	// // return the extension
-	return ext
+	// Get the path and extract the extension
+	return path.Ext(u.Path)
 }
